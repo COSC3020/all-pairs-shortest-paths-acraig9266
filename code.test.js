f6acb745be658192be92906075b6181a787a3665
Alex Craig <158104649+acraig9266@users.noreply.github.com>
2024-12-05 12:08:41 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -8,7 +8,7 @@ const graph1 = {
     B: [2,0,4],
     C: [1,3,0]
 }
-assert(JSON.stringify((graph1)) == JSON.stringify([[0, 2, 6], [2, 0, 4], [1, 3, 0]]));
+assert(JSON.stringify(allPairsShortestPaths(graph1)) == JSON.stringify([[0, 2, 6], [2, 0, 4], [1, 3, 0]]));
 
 const graph2 = {
     A: [0, 1, 2, 3],
@@ -16,4 +16,4 @@ const graph2 = {
     C: [4, 1, 0, 5],
     D: [5, 9, 9, 0]
 }
-assert(JSON.stringify((graph2)) == JSON.stringify([[0, 1, 2, 3], [2, 0, 3, 4], [3, 1, 0, 5], [5, 6, 7, 0]]));
+assert(JSON.stringify(allPairsShortestPaths(graph2)) == JSON.stringify([[0, 1, 2, 3], [2, 0, 3, 4], [3, 1, 0, 5], [5, 6, 7, 0]]));
